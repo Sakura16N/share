fd93903fca8f764275ea68aca39be63dcff81e61
ziweizhao <sakura16nn@163.com>
2025-04-18 10:31:37 +0800

    第四次提交 柜机类型的单表CRUD

--- share-modules/share-device/src/main/java/com/share/device/controller/CabinetTypeController.java
+++ share-modules/share-device/src/main/java/com/share/device/controller/CabinetTypeController.java
@@ -1,15 +1,14 @@
 package com.share.device.controller;
 
 import com.share.common.core.web.controller.BaseController;
+import com.share.common.core.web.domain.AjaxResult;
 import com.share.common.core.web.page.TableDataInfo;
 import com.share.device.domain.CabinetType;
 import com.share.device.service.ICabinetTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +32,48 @@ public class CabinetTypeController extends BaseController
         return getDataTable(list);
     }
 
+    /**
+     * 根据id查询柜机类型
+     */
+    @Operation(summary = "根据id查询柜机类型")
+    @GetMapping("/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id) {
+        CabinetType cabinetType = cabinetTypeService.getById(id);
+        return success(cabinetType);
+    }
 
+    /**
+     * 新增柜机类型
+     * @param cabinetType
+     * @return
+     */
+    @Operation(summary = "新增柜机类型")
+    @PostMapping
+    public AjaxResult add(@RequestBody CabinetType cabinetType) {
+        boolean isSaved = cabinetTypeService.save(cabinetType);
+        return toAjax(isSaved);
+    }
 
+    /**
+     * 根据id删除柜机类型
+     * @param ids
+     * @return
+     */
+    @Operation(summary = "根据id删除柜机类型")
+    @DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable List<Long> ids) {
+        boolean isDel = cabinetTypeService.removeBatchByIds(ids);
+        return toAjax(isDel);
+    }
+
+    /**
+     * 修改柜机类型
+     * @param cabinetType
+     * @return
+     */
+    @Operation(summary = "修改柜机类型")
+    @PutMapping
+    public AjaxResult update(@RequestBody CabinetType cabinetType) {
+        return toAjax(cabinetTypeService.updateById(cabinetType));
+    }
 }
