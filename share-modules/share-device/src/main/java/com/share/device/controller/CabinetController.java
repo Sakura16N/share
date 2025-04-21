05181288e97225b7ebc546b862c7818aad54c735
ziweizhao <sakura16nn@163.com>
2025-04-21 17:16:31 +0800

    第九次提交 实现充电宝柜机CRUD

--- share-modules/share-device/src/main/java/com/share/device/controller/CabinetController.java
+++ share-modules/share-device/src/main/java/com/share/device/controller/CabinetController.java
@@ -1,15 +1,15 @@
 package com.share.device.controller;
 
 import com.share.common.core.web.controller.BaseController;
+import com.share.common.core.web.domain.AjaxResult;
 import com.share.common.core.web.page.TableDataInfo;
 import com.share.device.domain.Cabinet;
 import com.share.device.service.ICabinetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +21,10 @@ public class CabinetController extends BaseController {
     @Autowired
     private ICabinetService cabinetService;
 
+    /**
+     * 分页查询柜机列表
+     * @return Ruo-yi框架封装的分页查询返回的类
+     */
     @Operation(summary = "分页查询柜机列表")
     @GetMapping("/list")
     public TableDataInfo list(Cabinet cabinet) {
@@ -28,4 +32,51 @@ public class CabinetController extends BaseController {
         List<Cabinet> list =  cabinetService.selectCabinetList(cabinet);
         return getDataTable(list);
     }
+
+    /**
+     * 根据Id查询指定柜机
+     */
+    @Operation(summary = "根据Id查询指定柜机")
+    @GetMapping("/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id) {
+        Cabinet cabinet = cabinetService.getById(id);
+        return AjaxResult.success(cabinet);
+    }
+
+    /**
+     * 新增充电宝柜机
+     * @param cabinet
+     * @return
+     */
+    @Operation(summary = "新增充电宝柜机")
+    @PostMapping()
+    public AjaxResult save(@RequestBody @Validated Cabinet cabinet) {
+        boolean isSaved = cabinetService.save(cabinet);
+        return toAjax(isSaved);
+    }
+
+    /**
+     * 根据id删除充电宝柜机
+     * @param id
+     * @return
+     */
+    @Operation(summary = "根据id删除充电宝柜机")
+    @DeleteMapping("{id}")
+    public AjaxResult remove(@PathVariable Long id) {
+        boolean isDeleted = cabinetService.removeById(id);
+        return toAjax(isDeleted);
+    }
+
+    /**
+     * 修改柜机
+     * @param cabinet
+     * @return
+     */
+    @Operation(summary = "修改柜机")
+    @PutMapping
+    public AjaxResult update(@RequestBody @Validated Cabinet cabinet) {
+        boolean isUpdated = cabinetService.updateById(cabinet);
+        return toAjax(isUpdated);
+    }
+
 }
